e558d02ddfd0413078adce65d4cc5bb4910f32a6
leonardoCavazzani <cavazzanileonardo@gmail.com>
2017-05-01 17:14:15 -0300

    pegando lista de videos do youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyAWBq_HuXZtHbLUCe5TEMOVl7gB8fGUMBM'
+
+YTSearch({key: API_KEY, term: 'League of legends'}, function (data) {
+  console.log(data);
+})
+
 
 const App = () => {
   return (
