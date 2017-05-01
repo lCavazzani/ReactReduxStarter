9eead96f200f159b1fc858a2c58db06cda00e2dc
leonardoCavazzani <cavazzanileonardo@gmail.com>
2017-05-01 14:03:30 -0300

    searchbar como classe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-const App = function () {
-  return <div>Hi!</div>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyAWBq_HuXZtHbLUCe5TEMOVl7gB8fGUMBM'
+
+const App = () => {
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
+
+ReactDOM.render(<App />, document.querySelector('.container'));
