7170fcb13f18875695e1c75a5763094bf86fb7e1
leonardoCavazzani <cavazzanileonardo@gmail.com>
2017-05-01 17:51:04 -0300

    usando state na funcao App

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,7 +14,7 @@ class SearchBar extends Component {
           <div>
           <input
           value={this.state.term}
-          onChange={event => this.setState({term: event.target.value})}/>;
+          onChange={event => this.setState({term: event.target.value})}/>
           Value of input: {this.state.term}
           </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyAWBq_HuXZtHbLUCe5TEMOVl7gB8fGUMBM'
 
-YTSearch({key: API_KEY, term: 'League of legends'}, function (data) {
-  console.log(data);
-})
+class App extends Component {
 
+  constructor(props){
+    super(props)
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+    this.state={videos: []}
+
+    YTSearch({key: API_KEY, term: 'League of legends'},(videos)=> {
+      this.setState({ videos })
+  //    this.setState({ videos: videos }) mesma coisa pq o nome das 2 variaveis é o mesmo
+      console.log(this.state.videos);
+    })
+  }
+  render(){
+    return (
+    <div>
+      <SearchBar />
+    </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
