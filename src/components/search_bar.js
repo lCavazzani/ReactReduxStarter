45fd285a7ae3de5667d22996d66bdf154ae71747
leonardoCavazzani <cavazzanileonardo@gmail.com>
2017-05-01 17:08:25 -0300

    usando state para manipular input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,22 @@ import React, {Component} from 'react';
 //import ReactDOM from 'react-dom';
 
 class SearchBar extends Component {
+
+    constructor(props){
+      super(props);
+
+      this.state = {term: ''};
+    }
+
     render(){
-        return <input />;
+        return (
+          <div>
+          <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}/>;
+          Value of input: {this.state.term}
+          </div>
+        );
     }
 }
 
