a67dcfe1c4c1245d91883823d9700d18bd36d9a5
leonardoCavazzani <cavazzanileonardo@gmail.com>
2017-05-01 21:26:25 -0300

    Pegando URL e titulo do video e colocando na lista de videos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyAWBq_HuXZtHbLUCe5TEMOVl7gB8fGUMBM'
@@ -22,6 +23,7 @@ class App extends Component {
     return (
     <div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />
     </div>
     );
   }
