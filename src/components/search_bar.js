b8cce88dfb1a4e64ab189606e1493bbb7d476053
leonardoCavazzani <cavazzanileonardo@gmail.com>
2017-05-13 22:24:50 -0300

    search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,7 +5,6 @@ class SearchBar extends Component {
 
     constructor(props){
       super(props);
-
       this.state = {term: ''};
     }
 
@@ -14,12 +13,19 @@ class SearchBar extends Component {
           <div>
           <input
           value={this.state.term}
-          onChange={event => this.setState({term: event.target.value})}/>
+          onChange={event => this.onInputChange(event.target.value)} />
           Value of input: {this.state.term}
           </div>
         );
     }
+    onInputChange(term){
+      this.setState({term})
+      this.props.onSearchTermChange(term)
+    }
+
 }
+
+
 
 // const SearchBar = () => {
 //   return <input />;
--- src/index.js
+++ src/index.js
@@ -16,8 +16,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     }
+    this.videoSearch('surfboards')
 
-    YTSearch({key: API_KEY, term: 'In cold blood'},(videos) => {
+  }
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term},(videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -29,7 +32,8 @@ class App extends Component {
   render(){
     return (
     <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)}
+ />
       <VideoDetail video={this.state.selectedVideo} />
       <VideoList
       onVideoSelect={selectedVideo => this.setState({selectedVideo})}
